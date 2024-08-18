fac88c90210706ea2be2466ba5144aa6a5712b57
mq61520 <qfncore@gmail.com>
2024-08-19 06:48:56 +0700

    before PWA

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '../html/ProfileProMax.html',
+          '../css/ProfileProMax.css',
+          '../js/main.js'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+  }
