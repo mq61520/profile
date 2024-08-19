d375ec7b91bd82315117b87a2d64e02bd2038b48
mq61520 <qfncore@gmail.com>
2024-08-19 08:37:55 +0700

    szvdv

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,7 @@ self.addEventListener('install', (event) => {
   });
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
     .then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
