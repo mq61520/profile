033d23f30939ab47f97d8f17a92b106b449a37d5
mq61520 <qfncore@gmail.com>
2024-08-19 08:40:32 +0700

    asdfbsdfb

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('v1').then((cache) => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/main.js',
-          '/icon-192x192.png',
-          '/icon-512x512.png'
+          '/profile.github.io/',
+          '/profile.github.io/index.html',
+          '/profile.github.io/style.css',
+          '/profile.github.io/main.js',
+          '/profile.github.io/icon-192x192.png',
+          '/profile.github.io/icon-512x512.png'
         ]);
       })
     );
@@ -22,7 +22,7 @@ self.addEventListener('install', (event) => {
   });
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js')
+    navigator.serviceWorker.register('/profile.github.io/service-worker.js')
     .then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
